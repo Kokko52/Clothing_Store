3303be5a07970445af6aa3153d7d67469c03dd12
Kokurin Egor <kokurinegorr@gmail.com>
2023-01-23 00:25:11 +0300

    Добавление форм: Menu, Authorization, Capcha

---
+++ magazin/Capcha.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace magazin
+{
+	partial class Capcha
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.next = new System.Windows.Forms.Button();
+			this.newone = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(38, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(300, 146);
+			this.pictureBox1.TabIndex = 8;
+			this.pictureBox1.TabStop = false;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(38, 177);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(300, 31);
+			this.textBox1.TabIndex = 9;
+			// 
+			// next
+			// 
+			this.next.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.next.Location = new System.Drawing.Point(201, 231);
+			this.next.Name = "next";
+			this.next.Size = new System.Drawing.Size(119, 47);
+			this.next.TabIndex = 12;
+			this.next.Text = "Продолжить";
+			this.next.UseVisualStyleBackColor = true;
+			// 
+			// newone
+			// 
+			this.newone.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.newone.Location = new System.Drawing.Point(60, 231);
+			this.newone.Name = "newone";
+			this.newone.Size = new System.Drawing.Size(119, 47);
+			this.newone.TabIndex = 11;
+			this.newone.Text = "Обновить";
+			this.newone.UseVisualStyleBackColor = true;
+			// 
+			// Capcha
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(376, 301);
+			this.Controls.Add(this.next);
+			this.Controls.Add(this.newone);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Capcha";
+			this.Text = "Capcha";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button next;
+		private System.Windows.Forms.Button newone;
+	}
+}
---
+++ magazin/Menu.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace magazin
+{
+	partial class Menu
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button7 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(384, 75);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(484, 359);
+			this.pictureBox1.TabIndex = 45;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button6
+			// 
+			this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button6.Location = new System.Drawing.Point(48, 454);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(821, 42);
+			this.button6.TabIndex = 44;
+			this.button6.Text = "Выйти из учетной записи";
+			this.button6.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button5.Location = new System.Drawing.Point(48, 119);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(279, 39);
+			this.button5.TabIndex = 43;
+			this.button5.Text = "Справочники";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button4.Location = new System.Drawing.Point(47, 229);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(280, 38);
+			this.button4.TabIndex = 42;
+			this.button4.Text = "Товары";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button3.Location = new System.Drawing.Point(47, 174);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(280, 39);
+			this.button3.TabIndex = 41;
+			this.button3.Text = "Пользователи";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button2.Location = new System.Drawing.Point(49, 284);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(280, 40);
+			this.button2.TabIndex = 40;
+			this.button2.Text = "Просмотр товаров";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(49, 339);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(279, 42);
+			this.button1.TabIndex = 39;
+			this.button1.Text = "Учет заказов";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.Location = new System.Drawing.Point(123, 75);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(138, 21);
+			this.label4.TabIndex = 38;
+			this.label4.Text = "Администратор";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(384, 23);
+			this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(31, 13);
+			this.label3.TabIndex = 37;
+			this.label3.Text = "апап";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(45, 75);
+			this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(73, 21);
+			this.label2.TabIndex = 36;
+			this.label2.Text = "Доступ:";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(45, 23);
+			this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(102, 21);
+			this.label1.TabIndex = 35;
+			this.label1.Text = "Сотрудник:";
+			// 
+			// button7
+			// 
+			this.button7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button7.Location = new System.Drawing.Point(51, 392);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(278, 42);
+			this.button7.TabIndex = 46;
+			this.button7.Text = "Новый заказ";
+			this.button7.UseVisualStyleBackColor = true;
+			// 
+			// Menu
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(915, 519);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Menu";
+			this.Text = "Menu";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button7;
+	}
+}
